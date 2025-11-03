e9b21bd34b2b30ffa6cac895df13d47c982f312d
DenisMishurin <dmishurin2008@gmail.com>
2025-11-04 01:23:01 +0300

    FIX

--- Gal/dynamical_modeling/LEDA_2220522/adamet_x_forstand_LEDA_2220522.py
+++ Gal/dynamical_modeling/LEDA_2220522/adamet_x_forstand_LEDA_2220522.py
@@ -449,9 +449,9 @@ def bestfit_adamet():
     print(f"VHalo = {bestfit[1]:0.2f} +/- {sig_bestfit[1]:0.2f}")
     print(pars)
 
-    return besfit, sig_bestfit
-besfit,sig_bestfit = bestfit_adamet()
-
+    return bestfit, sig_bestfit
+bestfit,sig_bestfit = bestfit_adamet()
+numpy.savetxt("bestfit.txt",[bestfit,sig_bestfit])
 numOrbits = 40000
 save_orb = True
 save_orbits_to = "" #[REQ].../Chemo-dynamical_modeling/LEDA_2220522
